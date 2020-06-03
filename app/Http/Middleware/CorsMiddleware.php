500e61bcc402f827c91e623b8e68333420a2c3e6
Bassel <basselkanso82@gmail.com>
2020-06-03 21:49:38 +0300

    Feature: Enable Cors
    -create CorsMiddleware in Middlware folder
    -enable CorsMiddlware in bootstrap/app.php

---
+++ app/Http/Middleware/CorsMiddleware.php
@@ -0,0 +1,25 @@
+<?php namespace App\Http\Middleware;
+
+use Closure;
+
+class CorsMiddleware
+{
+    public function handle($request, Closure $next)
+    {
+        $headers = [
+            'Access-Control-Allow-Origin'      => '*',
+            'Access-Control-Allow-Methods'     => 'POST, GET, PUT, DELETE',
+            'Access-Control-Allow-Credentials' => 'true',
+            'Access-Control-Max-Age'           => '86400',
+            'Access-Control-Allow-Headers'     => 'Content-Type, Authorization, X-Requested-With'
+        ];
+
+        $response = $next($request);
+        foreach($headers as $key => $value)
+        {
+            $response->header($key, $value);
+        }
+
+        return $response;
+    }
+}
